05b53931766b6246c2661dd4d3a2bf44bc62dd48
ditinagrawal <ditin.agrawal05@gmail.com>
2024-08-22 23:42:17 +0530

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,230 +1,127 @@
+import Link from "next/link";
+import React from "react";
+import { FaLinkedinIn, FaInstagram } from "react-icons/fa";
+
 export const Footer = () => {
   return (
-    <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 font-clarity">
-      <div className="grid gap-16 row-gap-10 mb-8 lg:grid-cols-6">
-        <div className="md:max-w-md lg:col-span-2">
-          <a
-            href="/"
-            aria-label="Go home"
-            title="Company"
-            className="inline-flex items-center"
-          >
-            <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
-              Dream <span className="text-primary1">Legal</span>
-            </span>
-          </a>
-          <div className="mt-4 lg:max-w-sm">
-            <p className="text-sm text-gray-800">
-              Dream Legal is your go-to directory for discovering the best legal software solutions. Designed specifically for legal professionals, Dream Legal helps you find and evaluate tools that streamline your practice, enhance efficiency, and support your unique needs. 
-              
-            </p>
-            <p className="mt-4 text-sm text-gray-800">
-              
-            </p>
-            <div className="flex items-center  space-x-4 mt-5">
-              <a
-                href="/"
-                className="text-primary1 bg-primary2 px-3 py-3 rounded-full transition-colors duration-300 hover:text-deep-purple-accent-400"
-              >
-                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
-                  <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z" />
-                </svg>
-              </a>
-              <a
-                href="/"
-                className="text-primary1 bg-primary2 px-3 py-3  rounded-full transition-colors duration-300 hover:text-deep-purple-accent-400"
-              >
-                <svg viewBox="0 0 30 30" fill="currentColor" className="h-6">
-                  <circle cx="15" cy="15" r="4" />
-                  <path d="M19.999,3h-10C6.14,3,3,6.141,3,10.001v10C3,23.86,6.141,27,10.001,27h10C23.86,27,27,23.859,27,19.999v-10   C27,6.14,23.859,3,19.999,3z M15,21c-3.309,0-6-2.691-6-6s2.691-6,6-6s6,2.691,6,6S18.309,21,15,21z M22,9c-0.552,0-1-0.448-1-1   c0-0.552,0.448-1,1-1s1,0.448,1,1C23,8.552,22.552,9,22,9z" />
-                </svg>
-              </a>
-              <a
-                href="/"
-                className="text-primary1 bg-primary2 px-3 py-3  rounded-full transition-colors duration-300 hover:text-deep-purple-accent-400"
-              >
-                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
-                  <path d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z" />
-                </svg>
-              </a>
+    <div className="bg-[#d8e2fc] w-full">
+      <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 font-clarity">
+        <div className="grid gap-16 row-gap-10 mb-8 lg:grid-cols-6">
+          <div className="md:max-w-md lg:col-span-2">
+            <a
+              href="/"
+              aria-label="Go home"
+              title="Company"
+              className="inline-flex items-center"
+            >
+              <span className="text-xl font-bold tracking-wide text-gray-800 uppercase">
+                About DreamLegal
+              </span>
+            </a>
+            <div className="mt-4 lg:max-w-sm">
+              <p className="text-sm text-gray-800">
+                We simplify selection, onboarding and management of technology
+                for legal professionals and teams.
+              </p>
+              <p className="mt-4 text-sm text-gray-800"></p>
+              <div className="flex items-center space-x-4 mt-5">
+                <a
+                  href="/"
+                  className="flex items-center bg-[#dc2f75] text-[#fff]  px-3 py-2 rounded-[30px] transition-all duration-300 hover:text-deep-purple-accent-400 hover:scale-110"
+                >
+                  <div className="flex gap-1 items-center">
+                    <FaInstagram size={18} color="#fff" />
+                    <p>Instagram</p>
+                  </div>
+                </a>
+                <a
+                  href="/"
+                  className=" flex items-center hover:scale-110 bg-[#2e64bc] text-white px-3 py-2  rounded-[30px] transition-all duration-300 hover:text-deep-purple-accent-400"
+                >
+                  <div className="flex gap-1 items-center">
+                    <FaLinkedinIn size={18} color="#fff" />
+                    <p>LinkedIn</p>
+                  </div>
+                </a>
+              </div>
+              <p className="mt-4 text-sm text-gray-800"></p>
+              <ul className="text-muted-foreground space-y-2 text-base">
+                <li>Home</li>
+                <li>Directory</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Privacy Policy</li>
+                <li>Terms and Conditions</li>
+              </ul>
+            </div>
+          </div>
+          <div className="md:max-w-md lg:col-span-2">
+            <a
+              href="/"
+              aria-label="Go home"
+              title="Company"
+              className="inline-flex items-center"
+            >
+              <span className="text-xl font-bold tracking-wide text-gray-800 uppercase">
+                Browse Categories
+              </span>
+            </a>
+            <div className="mt-4 lg:max-w-sm">
+              <ul className="text-muted-foreground space-y-2 text-base">
+                <li>Client Relationship Management</li>
+                <li>Contract Lifecycle Management</li>
+                <li>Document Management Software</li>
+                <li>E-Billing and Invoicing</li>
+                <li>E-Discovery</li>
+                <li>E-Signature</li>
+                <li>Governance, Risk and Compliance</li>
+                <li>Intellectual Property Management</li>
+                <li>Legal Research</li>
+                <li>Legal Workflow Automation</li>
+                <li>Litigation Management and Analytics</li>
+              </ul>
+            </div>
+          </div>
+          <div className="md:max-w-md lg:col-span-2">
+            <a
+              href="/"
+              aria-label="Go home"
+              title="Company"
+              className="inline-flex items-center"
+            >
+              <span className="text-xl font-bold tracking-wide text-gray-800 uppercase">
+                Users
+              </span>
+            </a>
+            <div className="mt-4 lg:max-w-sm">
+              <ul className="text-muted-foreground space-y-2 text-base">
+                <li>Enterprise</li>
+                <li>Government Departments</li>
+                <li>Individual Practitioner</li>
+                <li>Inhouse Counsel</li>
+                <li>Law Firm</li>
+                <li>Startups</li>
+              </ul>
+
+              <div className="mt-6">
+                <a
+                  href="/"
+                  className="bg-[#d8e2fc] border-2 border-[#333334] text-[#333334] px-3 py-3 rounded-[30px] 
+               transition-transform duration-300 ease-in-out transform hover:scale-[2]"
+                >
+                  Sign Up
+                </a>
+              </div>
             </div>
           </div>
         </div>
-        <div className="grid grid-cols-2 gap-5 row-gap-8 lg:col-span-4 md:grid-cols-4">
-          <div>
-            <p className="font-semibold tracking-wide text-gray-800">
-              Category
-            </p>
-            <ul className="mt-2 space-y-2">
-              <li>
-                <a
-                  href="/"
-                  className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                >
-                  News
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                >
-                  World
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                >
-                  Games
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                >
-                  References
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <p className="font-semibold tracking-wide text-gray-800">
-              Business
-            </p>
-            <ul className="mt-2 space-y-2">
-              <li>
-                <a
-                  href="/"
-                  className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                >
-                  Web
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                >
-                  eCommerce
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                >
-                  Business
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                >
-                  Entertainment
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                >
-                  Portfolio
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <p className="font-semibold tracking-wide text-gray-800">Apples</p>
-            <ul className="mt-2 space-y-2">
-              <li>
-                <a
-                  href="/"
-                  className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                >
-                  Media
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                >
-                  Brochure
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                >
-                  Nonprofit
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                >
-                  Educational
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                >
-                  Projects
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <p className="font-semibold tracking-wide text-gray-800">Cherry</p>
-            <ul className="mt-2 space-y-2">
-              <li>
-                <a
-                  href="/"
-                  className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                >
-                  Infopreneur
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                >
-                  Personal
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                >
-                  Wiki
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                >
-                  Forum
-                </a>
-              </li>
-            </ul>
-          </div>
+        <div className="flex flex-col justify-center pt-5 pb-10 border-t border-gray-400 sm:flex-row">
+          <p className="text-base text-gray-600">
+            Copyright 2024 —{" "}
+            <Link href={"https://dreamlegal.in/"}>dreamlegal.in</Link>. All
+            rights reserved.
+          </p>
         </div>
-      </div>
-      <div className="flex flex-col justify-between pt-5 pb-10 border-t sm:flex-row">
-        <p className="text-sm text-gray-600">
-          © Copyright 2024 Lorem Inc. All rights reserved.
-        </p>
       </div>
     </div>
   );

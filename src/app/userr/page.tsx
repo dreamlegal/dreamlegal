f5d90705ae559227d451f68bb77fbd8b939ccce2
Rohan Kumar <rohanvkumarv@gmail.com>
2024-11-18 15:34:16 +0530

    build error fixed

--- src/app/userr/page.tsx
+++ src/app/userr/page.tsx
@@ -1,8 +1,21 @@
 
 
-import { redirect } from "next/navigation";
-export default function UserPage() {
-    redirect("/auth/user/login");
+// import { redirect } from "next/navigation";
+// export default function UserPage() {
+//     redirect("/auth/user/login");
     
-}
-// return <div> Hello</div>;
+// }
+// // return <div> Hello</div>;
+// /userr/page.js
+"use client"
+export default function UserPage() {
+    return (
+      <div>
+        <h1>User Page</h1>
+        <p>
+          Please <a href="/auth/user/login">click here</a> to log in.
+        </p>
+      </div>
+    );
+  }
+  

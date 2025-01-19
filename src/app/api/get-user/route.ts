3b5c09d0f41f032dba5b451c4d2944e43ac3b52a
Rohan Kumar <rohanvkumarv@gmail.com>
2025-01-19 23:33:16 +0530

    bug fixed

--- src/app/api/get-user/route.ts
+++ src/app/api/get-user/route.ts
@@ -1,6 +1,6 @@
 import prisma from "@/lib/prisma";
 import { NextRequest } from "next/server";
-
+export const dynamic = 'force-dynamic';
 export async function GET(request: NextRequest) {
   try {
     const searchParams = request.nextUrl.searchParams;
---
+++ src/app/legal_professionals/dashboard/book_my_calls/page.tsx
@@ -0,0 +1,8 @@
+// Server Component (default in Next.js 13+)
+export default function BookMyCallsPage() {
+    return (
+      <div>
+        <h1>Book My Calls</h1>
+      </div>
+    );
+  }
